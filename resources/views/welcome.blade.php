157b9b6aee731a2a1816daa58c98422ab6eb933b
A. S. Md. Ferdousul Haque <ferdousul.haque@gmail.com>
2019-01-02 00:31:55 +0600

    Resolved Server Side Rendering

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,79 @@
-<html>
-<head>
-    <title>My server side rendered app</title>
-    <script defer src="{{ mix('js/app-client.js') }}" />
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>My server side rendered app</title>
 
-</head>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-<body>
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
 
-{!! ssr('js/app-server.js')->render() !!}
+            .full-height {
+                height: 100vh;
+            }
 
-</body>
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 68px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel Server Side <br> Rendering
+                </div>
+                <div class="links">
+                    <a href="{{ url('vue') }}">Vue</a>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,63 @@
+
+const path = require('path');
+const glob = require('glob-all');
 const mix = require('laravel-mix');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+    |--------------------------------------------------------------------------
+    | Mix Asset Management
+    |--------------------------------------------------------------------------
+    |
+    | Mix provides a clean, fluent API for defining some Webpack build steps
+    | for your Laravel application. By default, we are compiling the Sass
+    | file for the application as well as bundling up all the JS files.
+    | .sass('resources/sass/app.scss', 'public/css')
+    */
 
-mix.js('resources/js/app-client.js', 'public/js')
-    .js('resources/js/app-server.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix
+    .js('resources/assets/js/entry-client.js', 'public/js')
+    .js('resources/assets/js/entry-server.js', 'public/js')
+
+    .postCss('resources/assets/css/app.css', 'public/css/app.css', [
+        require('tailwindcss')('./tailwind.js'),
+    ])
+
+    
     .browserSync({"proxy":"localhost:8000"})
+
+    .webpackConfig(() => {
+        const config = {};
+
+        config.resolve = {
+            alias: {
+                vue$: 'vue/dist/vue.runtime.common.js',
+            },
+        };
+
+        if (mix.inProduction()) {
+            config.plugins = [
+                new PurgecssPlugin({
+                    paths: glob.sync([
+                        path.join(__dirname, 'app/**/*.php'),
+                        path.join(__dirname, 'resources/views/**/*.blade.php'),
+                        path.join(__dirname, 'resources/assets/js/**/*.vue'),
+                        path.join(__dirname, 'resources/assets/js/**/*.js'),
+                    ]),
+                    extractors: [
+                        {
+                            extractor: class {
+                                static extract(content) {
+                                    return content.match(/[A-z0-9-:\/]+/g) || [];
+                                }
+                            },
+                            extensions: ['html', 'js', 'php', 'vue'],
+                        },
+                    ],
+                }),
+            ];
+        }
+
+        return config;
+    });
+    
